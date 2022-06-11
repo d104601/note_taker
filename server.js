242bcbe708bce9dec3f17cd8b05995b90b6040bb
Taeyong Lee <d104601@hotmail.com>
2022-06-11 01:28:00 -0400

    fixed bug

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const dbFile = require("./db/db.json");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db.json");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 //return the "notes.html"
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
-//return the "index.html"
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
 app.get("/api/notes", (req, res) => {
-    console.log(`${req.method} request recieved to get a note`);
-    return res.json(dbFile);
+    return res.json(database);
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
     
 app.listen(PORT, () => 
     console.log(`App is listening at http://localhost:${PORT}`)
