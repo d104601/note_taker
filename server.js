216c6c4e7956af53637c5e382ba72e873fca8a67
Taeyong Lee <d104601@hotmail.com>
2022-06-10 23:20:03 -0400

    heroku test

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const db = require("./db/db.json");
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //return the "notes.html"
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
@@ -12,10 +16,12 @@ app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.
 //return the "index.html"
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
-app.get("/api/notes", (req, res) => )
+
+app.route("/api/notes")
+    .get((req, res) => res.json(JSON.parse(db)));
 
 // note posting
-app.post();
+
 
 app.listen(PORT, () => 
     console.log(`App is listening at http://localhost:${PORT}`)
