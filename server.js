43268ed4ea7be39324c652187c8326d0cbc51e6a
Taeyong Lee <d104601@hotmail.com>
2022-06-11 00:55:52 -0400

    get test

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const db = require("./db/db.json");
+const dbFile = require("./db/db.json");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 //return the "notes.html"
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
@@ -17,27 +17,10 @@ app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.
 //return the "index.html"
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
 
-
-app.route("/api/notes")
-    .get(function(req, res) {
-        res.json(db);
-    })
-    // note posting
-    .post(function(req, res) {
-        let jsonPath = path.join(__dirname, "db/db.json");
-        let newNote = req.body;
-
-        db.push(newNote);
-
-        fs.writeFile("db/db.json", db, (err) =>{
-            if(err)
-            {
-                throw err;
-            }
-            console.log("Note saved.");
-        })
-    });
-
+app.get("/api/notes", (req, res) => {
+    console.log(`${req.method} request recieved to get a note`);
+    return res.json(dbFile);
+});
     
 app.listen(PORT, () => 
     console.log(`App is listening at http://localhost:${PORT}`)
